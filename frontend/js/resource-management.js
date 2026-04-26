845569f7da0907cffd6c6b7231254c30b86b6a93
AreejAljuhani <Areejsalj@gmail.com>
2026-04-26 12:39:54 +0300

    Add resource management page with Firebase integration for doctors and beds control

---
+++ frontend/js/resource-management.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const db = firebase.firestore();
+
+    function loadResources() {
+        db.collection("settings").doc("resources").get().then(doc => {
+
+            if (doc.exists) {
+                const data = doc.data();
+
+                document.getElementById("doctorsInput").value = data.doctorsAvailable || 0;
+                document.getElementById("bedsInput").value = data.bedsAvailable || 0;
+
+                document.getElementById("doctorsCount").innerText = data.doctorsAvailable || 0;
+                document.getElementById("bedsCount").innerText = data.bedsAvailable || 0;
+            }
+
+        }).catch(err => console.log(err));
+    }
+
+    db.collection("settings").doc("resources")
+    .onSnapshot(doc => {
+        if (doc.exists) {
+            const data = doc.data();
+
+            document.getElementById("doctorsCount").innerText = data.doctorsAvailable || 0;
+            document.getElementById("bedsCount").innerText = data.bedsAvailable || 0;
+        }
+    });
+
+    window.updateDoctors = function () {
+        let val = parseInt(document.getElementById("doctorsInput").value || 0);
+
+        db.collection("settings").doc("resources").set({
+            doctorsAvailable: val
+        }, { merge: true });
+    }
+
+    window.updateBeds = function () {
+        let val = parseInt(document.getElementById("bedsInput").value || 0);
+
+        db.collection("settings").doc("resources").set({
+            bedsAvailable: val
+        }, { merge: true });
+    }
+
+    loadResources();
+
+});
